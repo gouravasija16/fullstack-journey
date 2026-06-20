b29b5138f00521439870638f625e73165dd3bc1b
Gourav <gouravasija1604@gmail.com>
2026-06-20 11:06:46 +0530

    Updated functions in JavaScipt

--- week2/day1.js
+++ week2/day1.js
@@ -1,26 +1,26 @@
-console.log("JavaScript journey started now");
-let name="gourav";
-const year=2026;
-var college= "TITS Bhiwani";
-console.log(name);
-console.log(year);
-console.log(college);
+console.log("JavaScript journey started now")
+let name="gourav"
+const year=2026
+var college= "TITS Bhiwani"
+console.log(name)
+console.log(year)
+console.log(college)
 
- function getName(){
-    console.log("JavaScript");
+function getName(){
+    console.log("JavaScript")
 }
-console.log(getName);
-console.log(x);
-//console.log(y);
-let y=10;
-var x=7;
+console.log(getName)
+console.log(x)
+//console.log(y)
+let y=10
+var x=7
 
-console.log(typeof(name));
-let isStudent=true;
-console.log(typeof(isStudent));
-console.log(typeof(year));
-console.log(typeof(Math));
-console.log(`sum of x and y is ${x+y}`);
+console.log(typeof(name))
+let isStudent=true
+console.log(typeof(isStudent))
+console.log(typeof(year))
+console.log(typeof(Math))
+console.log(`sum of x and y is ${x+y}`)
 
 
 
--- week2/day2.js
+++ week2/day2.js
@@ -61,8 +61,84 @@ console.log(reverse)
 let number1=prompt("Enter a number")
 for (let i=0;i<number1;i++){
     console.log(i)
+// }
+//Square of a function
+let getSquare=n =>n*n
+let n=prompt("Enter a number:")
+console.log(getSquare(n))
+//Check if a number is even or odd
+function findEvenOdd(n){
+    if(n%2==0) return "Even"
+    else return "Odd";
 }
-
-
-
-
+let num=prompt("Enter a number:")
+console.log(findEvenOdd(num))
+//find greater of two numbers
+function greater(a,b){
+    if(a>b) return `${a} is greater`
+    else return `${b} is greater`
+}
+let a= prompt("Enter number1:")
+let b= prompt("Enter number2:")
+console.log(greater(a,b))
+//Temperature converter 
+const celsiusToFahrenheit=(C)=>(C*(9/5))+32
+const fahrenheitToCelsius=(F)=>((F-32)*(5/9))
+console.log(celsiusToFahrenheit(70))
+console.log(fahrenheitToCelsius(250))
+//Find Factorial using recurssion
+function findFactorial(n){
+    if (n===0) return 1
+    return n * findFactorial(n-1) 
+}
+let n=prompt("Enter a number:")
+console.log(`Factorial of ${n} is ${findFactorial(n)}`)
+//Sum of digits of a number
+function sumOfdigits(n){
+    let sumofdigits=0
+    while(n>0){
+        sumofdigits+=n%10
+        n=Math.floor(n/10)
+    }
+    return sumofdigits
+}
+let n=prompt("Enter a number:")
+console.log(`sum of digits of ${n} is ${sumOfdigits(n)}`)
+//Check if number is palindrome
+function isPalindrome(n){
+    let ispalindrome=false
+    let original=n
+    if(original == Reverse(original)) ispalindrome=true
+    return ispalindrome
+}
+function Reverse(num){
+let reverse=0;
+while(num>0){
+    reverse=reverse*10+num%10
+     num=Math.floor(num/10)
+}
+return reverse
+}
+let n=prompt("Enter a number:")
+console.log(isPalindrome(n))
+// Count vowels in a string 
+function countVowels(str){
+    let count=0
+    let Vowels="aeiouAEIOU"
+    for(let i=0;i<str.length;i++){
+        if(Vowels.includes(str[i])){
+            count++
+        }
+    }
+    return count
+}
+console.log(countVowels("hello world"))
+// Find maximum in an array
+function findMax(arr){
+    let max=arr[0]
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>max) max=arr[i]
+    }
+    return max
+}
+console.log(findMax([1,5,9,10,32,9,7]))
