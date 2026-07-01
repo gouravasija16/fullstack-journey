d52385e0a7bc80a37dad5cab6da7f2196c9a35db
Gourav <gouravasija1604@gmail.com>
2026-07-01 14:09:59 +0530

    Added functionality of filter buttons and edit button

--- projects/To-do list/to-do.js
+++ projects/To-do list/to-do.js
@@ -1,4 +1,5 @@
 const tasks=[]
+let editIndex=null
 const taskInput=document.getElementById("added-tasks")
 const addBtn=document.getElementById("add")
 const prioritySelect=document.getElementById("priority")
@@ -7,7 +8,8 @@ const display=document.getElementById("display")
 
 function renderTasks(){
     display.innerHTML=""
-    tasks.forEach((task,index)=> {
+    const  filteredTask=tasks.filter(filterTask)
+       filteredTask.forEach((task,index)=> {
         const taskCard=document.createElement("div")
         taskCard.classList.add("task-card")
         taskCard.innerHTML=
@@ -19,17 +21,32 @@ function renderTasks(){
          <button id="edit-Btn" onclick="editTask(${index})">✏️</button>
         <button id="delete-Btn" onclick="deleteTask(${index})">🗑️</button>  
          `
+         display.appendChild   (taskCard)
     })
 }
 addBtn.addEventListener("click",function(){
-    const priority=prioritySelect.value
+        
+     if(editIndex !==null){
+        
+         tasks[editIndex]={
+            task:taskInput.value.trim(),
+            priority:prioritySelect.value,
+            dueDate:dueDateInput.value,
+            complete:tasks[editIndex].complete
+        } 
+        tasks.splice(editIndex,1,tasks[editIndex])
+         editIndex=null
+          taskInput.value=""
+        addBtn.textContent="Add"    
+    }else{
+         const priority=prioritySelect.value
     const dueDate=dueDateInput.value
      const input=taskInput.value.trim();
      if(input===""){
         alert("Enter a valid value")
         return
      }
-         let taskObject={
+      let taskObject={
             task:input,
             priority:priority,
             dueDate:dueDate,
@@ -37,10 +54,64 @@ addBtn.addEventListener("click",function(){
      }
      tasks.push(taskObject)
      taskInput.value=""
+    }
       renderTasks()
 })
  function deleteTask(index){
     tasks.splice(index,1)
     renderTasks()
  }
+ function editTask(index){
+    taskInput.value=tasks[index].task
+    prioritySelect.value=tasks[index].priority
+    dueDateInput.value=tasks[index].dueDate
+    editIndex=index
+    document.getElementById("add").textContent="Update"
+ }
+ const mode=document.getElementById("mode")
+ mode.addEventListener("click",function(){
+   document.body.classList.toggle("dark")
+   if(document.body.className==="dark"){
+   mode.textContent="☀️ Light Mode"
+   }else{
+     mode.textContent="🌙 Dark Mode"
+   }
+ })
+ let currentFilter="all" 
+ document.querySelectorAll("#filterBtn button").forEach((Btn)=>{
+    Btn.addEventListener("click",function(){
+       currentFilter= Btn.dataset.filter
+         document.querySelectorAll("#filterBtn button").forEach((btn)=>{
+            btn.style.backgroundColor="white"
+            btn.style.color="black"
+         })
+
+       Btn.style.backgroundColor="#0EA5E9"
+       Btn.style.color="white"
+       console.log(currentFilter)
+       renderTasks()
+        
+    })
+ })
+ function filterTask (task){
+    switch(currentFilter){
+        case "completed":
+         return task.complete===true
+        case "active":
+         return task.complete===false
+       
+        case "high":
+            return  task.priority.toLowerCase()===currentFilter
+        case "medium":
+              return task.priority.toLowerCase()===currentFilter
+        case "low":
+             return  task.priority.toLowerCase()===currentFilter
+        default :
+              return true
+           
+    }
+ }
+
+
+
  
